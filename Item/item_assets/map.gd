e601652aec29b2945c113536937371a1c0a51721
BRYAN <bryanwang105@gmail.com>
2024-03-10 23:20:49 -0700

    MAP ITEM: Concept for a map item but was breaking market feature

---
+++ Item/item_assets/map.gd
@@ -0,0 +1,58 @@
+extends RigidBody3D
+
+@export var slot_data: SlotData
+@onready var map_panel: Panel = $MapMenu/mapPanel
+
+var item = true
+var prompt = null
+var player
+var world
+
+func ready():
+	world = get_parent()
+	player = world.player
+	
+func get_prompt(needSwap) -> String:
+	if needSwap:
+		prompt = "Press E to swap item for " + slot_data.item_data.name
+	else:
+		prompt = "Press E to pick up " + slot_data.item_data.name
+		
+	return prompt
+
+func interact(needSwap) -> SlotData:
+	if needSwap:
+		# free item
+		queue_free()
+		
+		# Instantiate new item
+		world = get_parent()
+		player = world.player
+	
+		var drop_item = load(player.current_slot.item_data.pathToAsset)
+		var drop_node = drop_item.instantiate()
+		
+		# Spawn replaced item to the grabbed coordinates
+		drop_node.position = position
+		world.add_child(drop_node)
+
+	else:
+		print("Picked up map!")
+		queue_free()
+	
+	return slot_data
+	
+
+static func use(player: CharacterBody3D):
+	print("Read Map")
+	
+	player.world._toggle_map()
+	
+	print("123")
+	# Will return true to signal consume / destroy item
+	return false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
